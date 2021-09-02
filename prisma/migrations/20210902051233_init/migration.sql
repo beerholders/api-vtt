bcb336c518fe9ea4d1351a050ee66b09b8cc267a
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-02 01:59:00 -0300

    Add password to users

--- prisma/migrations/20210902051233_init/migration.sql
+++ prisma/migrations/20210902051233_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191),
 
     UNIQUE INDEX `User.email_unique`(`email`),
