be6514d86a79ac77d13db21cfb60d3628a6cd21a
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-04 00:28:56 -0300

    Add /api/logout

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { loginRouter } from "./login";
+import { logoutRouter } from "./logout";
 import { signupRouter } from "./signup";
 import { sessionRouter } from "./session";
 import { usersRouter } from "./users";
@@ -9,6 +10,7 @@ const apiRouter = Router();
 
 apiRouter.use(getValidateAuthSessionWithExceptions(["/login", "/signup"]));
 apiRouter.use("/login", loginRouter);
+apiRouter.use("/logout", logoutRouter);
 apiRouter.use("/signup", signupRouter);
 apiRouter.use("/session", sessionRouter);
 apiRouter.use("/users", usersRouter);
