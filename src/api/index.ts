d5a9b5eaa5e28f0723fbb923cf01dbb196be2eed
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-04 00:04:29 -0300

    Add /api/session

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { loginRouter } from "./login";
 import { signupRouter } from "./signup";
+import { sessionRouter } from "./session";
 import { usersRouter } from "./users";
 import { getValidateAuthSessionWithExceptions } from "@/middlewares/auth";
 
@@ -9,6 +10,7 @@ const apiRouter = Router();
 apiRouter.use(getValidateAuthSessionWithExceptions(["/login", "/signup"]));
 apiRouter.use("/login", loginRouter);
 apiRouter.use("/signup", signupRouter);
+apiRouter.use("/session", sessionRouter);
 apiRouter.use("/users", usersRouter);
 
 export { apiRouter };
