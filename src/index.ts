89b62704f15b1ed5e738d913bd93ecf46b85069c
Gabriel Dutra <nesk.frz@gmail.com>
2021-09-04 13:40:55 -0300

    Use $PORT env var

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import { apiRouter } from "./api";
 import { handleHttpErrors } from "./middlewares/handleHttpErrors";
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.json());
@@ -11,8 +13,8 @@ app.use(cookieParser());
 app.use("/api", apiRouter);
 app.use(handleHttpErrors);
 
-app.listen(3001, () =>
+app.listen(PORT, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3001
+🚀 Server ready at: http://localhost:${PORT}
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
 );
